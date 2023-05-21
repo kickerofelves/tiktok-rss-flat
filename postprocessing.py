c48f7b796b6c3268076d6ebbe53bcecc0dd04411
1aaronscott <1aaronscott@users.noreply.github.com>
2023-05-20 21:19:49 -0500

    Added some error logging

--- postprocessing.py
+++ postprocessing.py
@@ -23,67 +23,100 @@ start_range = hour*block_size
 end_range = start_range+block_size
 
 
+def log(message):
+    print(f"-------------------- { message } --------------------")
+
+
+def logError(exception):
+    log("!!!!! ERROR !!!!!")
+    log(exception)
+
+
 async def runAll():
-    with open('subscriptions.csv') as f:
-        reader = csv.reader(f)
-        name_list = list(reader)
-
-        # TODO: Switch to 3.11 TaskGroup or trio nursery
-        await asyncio.gather(*[
-            # run(row['username']) for row in csv.DictReader(f, fieldnames=['username'])])
-            run(row[0] for row in name_list[start_range:end_range])])
-
-
-async def run(csvuser):
     try:
-        print(f'Running for user \'{csvuser}\'')
+        log("runAll start")
+        log("attempt to load subscription file number to run")
+        with open('subscriptions.csv') as f:
+            reader = csv.reader(f)
+            name_list = list(reader)
 
-        fg = FeedGenerator()
-        fg.id('https://tiktok.com/@' + csvuser)
-        fg.title(csvuser + ' TikTok')
-        fg.author({'name': 'Conor ONeill', 'email': 'conor@conoroneill.com'})
-        fg.link(href='http://tiktok.com', rel='alternate')
-        fg.logo(ghPagesURL + 'tiktok-rss.png')
-        fg.subtitle('OK Boomer, all the latest TikToks from ' + csvuser)
-        fg.link(href=ghPagesURL + 'rss/' + csvuser + '.xml', rel='self')
-        fg.language('en')
+            # TODO: Switch to 3.11 TaskGroup or trio nursery
+            await asyncio.gather(*[
+                # run(row['username']) for row in csv.DictReader(f, fieldnames=['username'])])
+                run(row[0] for row in name_list[start_range:end_range])])
+    except Exception as e:
+        logError(e)
+
+
+async def run(tiktokUsername):
+    log(f"run start ( { tiktokUsername } )")
+    try:
+        feedGenerator = FeedGenerator()
+        feedGenerator.id("https://www.tiktok.com/@" + tiktokUsername)
+        feedGenerator.title("@" + tiktokUsername + " | TikTok")
+        feedGenerator.author(
+            {"name": "Conor ONeill", "email": "conor@conoroneill.com"})
+        feedGenerator.link(href="https://www.tiktok.com/@" +
+                           tiktokUsername, rel="alternate")
+        feedGenerator.logo(ghPagesURL + "tiktok-rss.png")
+        feedGenerator.subtitle("Latest TikToks from @" + tiktokUsername)
+        feedGenerator.link(href=ghPagesURL + "rss/" +
+                           tiktokUsername + ".xml", rel="self")
+        feedGenerator.language("en")
 
         # Set the last modification time for the feed to be the most recent post, else now.
         updated = None
 
         async with AsyncTikTokAPI(navigation_retries=3, navigation_timeout=60) as api:
-            tiktokuser = await api.user(csvuser, video_limit=maxItems)
-            async for video in tiktokuser.videos:
-                # print(video.create_time, video.desc)
-                print("URL = " + "https://tiktok.com/@" +
-                      csvuser + "/video/" + str(video.id))
-                fe = fg.add_entry()
-                link = "https://tiktok.com/@" + \
-                    csvuser + "/video/" + str(video.id)
-                fe.id(link)
-                ts = video.create_time
-                print(ts)
-                fe.published(ts)
-                fe.updated(ts)
-                updated = max(ts, updated) if updated else ts
-                if video.desc:
-                    fe.title(video.desc[0:255])
-                else:
-                    fe.title("No Title")
-                fe.link(href=link)
-                # fe.description("<img src='" + tiktok.as_dict['video']['cover'] + "' />")
-                if video.desc:
-                    fe.description(video.desc)
-                else:
-                    fe.description("No Description")
-                # print(fg.rss_str(pretty=True))
+            tiktokUser = await api.user(tiktokUsername, video_limit=maxItems)
+            async for video in tiktokUser.videos:
+                log(f"processing video from @{ tiktokUsername }")
+                try:
+                    videoLink = f"https://www.tiktok.com/@{ tiktokUsername }/video/{ str( video.id ) }"
+                    feedEntry = feedGenerator.add_entry()
+                    feedEntry.id(videoLink)
 
-        fg.updated(updated)
+                    timestamp = video.create_time
+                    # print( timestamp )
+                    feedEntry.published(timestamp)
+                    feedEntry.updated(timestamp)
+                    updated = max(timestamp, updated) if updated else timestamp
+
+                    if video.desc:
+                        feedEntry.title(video.desc[0:255])
+                    else:
+                        feedEntry.title("[no title]")
+
+                    feedEntry.link(href=videoLink)
+
+                    # feedEntry.description( "<img src = "" + tiktok.as_dict["video"]["cover"] + "" />" )
+                    if video.desc:
+                        feedEntry.description(video.desc)
+                    else:
+                        feedEntry.description("[no description]")
+
+                    log(
+                        f"successfully processed video from @{ tiktokUsername }")
+
+                except Exception as e:
+                    log(
+                        f"error occurred while processing video from @{ tiktokUsername }")
+                    logError(e)
+                    continue
+
+        feedGenerator.updated(updated)
+
+        feedFileName = "rss/" + tiktokUsername + ".xml"
+        log(f"attempt to write feed to file { feedFileName }")
         # Write the RSS feed to a file
-        fg.atom_file('rss/' + csvuser + '.xml', pretty=True)
+        feedGenerator.atom_file(feedFileName, pretty=True)
+
     except Exception as e:
-        print(f"Some error: {e}")
+        log("error occurred outside of processing videos")
+        logError(e)
         pass
+
+    log(f"run end ( { tiktokUsername } )")
 
 
 asyncio.run(runAll())
